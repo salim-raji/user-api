b1d4830c3830372e0eb99bb354435853cdda5432
salim <salimraji@icloud.com>
2024-10-09 11:57:54 +0300

    latest

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require("mongodb");
 
 let dbConnection;
-const uri = 'mongodb+srv://salimraji:1234@mycluster.lbrtq.mongodb.net/?retryWrites=true&w=majority&appName=myCluster'
+const uri = 'mongodb+srv://salimraji:1234@mycluster.lbrtq.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=myCluster'
 module.exports={
     connectToDb: (cb) => {
         MongoClient.connect(uri)
